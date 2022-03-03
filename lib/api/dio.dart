074c9154fc9796caad65f8ad1475d367af14773b
watanavex <watanave.tech@gmail.com>
2022-03-04 06:24:24 +0900

    Add dio provider

---
+++ lib/api/dio.dart
@@ -0,0 +1,14 @@
+// 📦 Package imports:
+import 'package:dio/dio.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final dioProvider = Provider((ref) {
+  final dio = Dio();
+
+  // flutter run --debug --dart-define=API_TOKEN={YOUR_GITHUB_API_TOKEN}
+  const apiToken = String.fromEnvironment('API_TOKEN');
+  if (apiToken.isNotEmpty) {
+    dio.options.headers['Authorization'] = 'token $apiToken';
+  }
+  return dio;
+});
