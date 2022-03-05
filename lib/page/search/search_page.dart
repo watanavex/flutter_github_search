356a51828db9beffd11d62af027f811bc6ac51b1
watanavex <watanave.tech@gmail.com>
2022-03-05 09:59:13 +0900

    Add page of search

---
+++ lib/page/search/search_page.dart
@@ -0,0 +1,14 @@
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+
+// 📦 Package imports:
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SearchPage extends HookConsumerWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
