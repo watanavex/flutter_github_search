1a64e8930f7ab66f9741d1c370c6c28e8773998c
watanavex <watanave.tech@gmail.com>
2022-03-05 15:54:55 +0900

    Apply lint rule

--- lib/ui/component/circle_image_view.dart
+++ lib/ui/component/circle_image_view.dart
@@ -1,13 +1,8 @@
-// 🎯 Dart imports:
-import 'dart:async';
-
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
 // 📦 Package imports:
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class CircleImageView extends StatelessWidget {
   const CircleImageView({
