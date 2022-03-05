1b5df4050a845a0f8225060a0a3179fc6821c73a
watanavex <watanave.tech@gmail.com>
2022-03-05 10:02:52 +0900

    Add list view to search page

--- lib/page/search/search_page.dart
+++ lib/page/search/search_page.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // 🌎 Project imports:
+import 'package:flutter_github_search/component/circle_image_view.dart';
+import 'package:flutter_github_search/component/pagination_list_view.dart';
+import 'package:flutter_github_search/page/search/search_page_state.dart';
 import 'package:flutter_github_search/page/search/search_page_state_notifier.dart';
 
 class SearchPage extends HookConsumerWidget {
@@ -29,16 +32,67 @@ class SearchPage extends HookConsumerWidget {
         return const Text('searching');
       },
       success: (repositories, query, page, haxNext) {
-        return const Text('success');
+        return _buildListView(context, ref, repositories, haxNext);
       },
       fetchingNext: (repositories, query, page) {
-        return const Text('fetchingNext');
+        return _buildListView(context, ref, repositories, true);
       },
       fail: () {
         return const Text('fail');
       },
       empty: () {
         return const Text('empty');
+      },
+    );
+  }
+
+  Widget _buildListView(BuildContext context, WidgetRef ref,
+      List<RepositorySummary> repositories, bool hasNext) {
+    final notifier = ref.read(searchPageStateNotifierProvider.notifier);
+    return Scrollbar(
+      child: NotificationListener<ScrollNotification>(
+        child: PaginationListView(
+          itemCount: repositories.length,
+          hasNext: hasNext,
+          fetchNext: notifier.fetchNext,
+          itemBuilder: (context, index) {
+            return _buildListItems(context, ref, repositories[index]);
+          },
+        ),
+        onNotification: (notification) {
+          if (notification.metrics.atEdge &&
+              notification.metrics.extentAfter == 0) {
+            notifier.fetchNext();
+          }
+          return false;
+        },
+      ),
+    );
+  }
+
+  Widget _buildListItems(
+      BuildContext context, WidgetRef ref, RepositorySummary item) {
+    const leadingSize = 56.0;
+    const placeholder = Icon(
+      Icons.person,
+      size: leadingSize,
+    );
+    return ListTile(
+      leading: CircleImageView(
+        imageUrl: item.imageUrl,
+        placeholder: () => placeholder,
+      ),
+      title: Text(
+        item.name,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      subtitle: Text(
+        item.owner,
+        style: Theme.of(context).textTheme.caption,
+      ),
+      minLeadingWidth: leadingSize,
+      onTap: () {
+        debugPrint('tap list item');
       },
     );
   }
